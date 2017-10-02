12237922d319d0c51ed5e9b5843b515ed40fda6a
KittyCervantes <bcervantes9@ecc-jaguar.csuchico.edu>
2017-10-02 16:40:20 -0700

    decomposition

--- main.cpp
+++ main.cpp
@@ -6,7 +6,15 @@
 
 int main()
 {
-
+//ask for bmp formated image from user
+    //error message if other than bmp until valid picture provided
+//save input
+//convert pixel to grey scale equivalents
+    //find average of component red, green and blue
+    //assign rgb average value
+    //repeat until all pixels are converted in picture
+//save new picture as oldtimey.bmp
+//show new picture as output
 
 
 return 0;
