b35153262e57c1e25869ea517a44d5295300c5e7
KittyCervantes <bcervantes9@ecc-jaguar.csuchico.edu>
2017-10-05 19:01:05 -0700

    I have no Idea

--- main.cpp
+++ main.cpp
@@ -4,18 +4,49 @@
 #include<vector>
 #include"bitmap.h"
 
+
 int main()
 {
-//ask for bmp formated image from user
-    //error message if other than bmp until valid picture provided
-//save input
-//convert pixel to grey scale equivalents
-    //find average of component red, green and blue
-    //assign rgb average value
-    //repeat until all pixels are converted in picture
-//save new picture as oldtimey.bmp
-//show new picture as output
+        Pixel rgb;
+        Bitmap image;
+        vector <vector <Pixel> > bmp;
 
 
-return 0;
+        //ask for bmp formated image from user
+
+
+        do{
+        cout<<"Please upload valid bmp image: "<<endl;
+        cin>>image<<endl;
+        }
+        while(image!=bmp) //check to see if thats right
+        
+
+        image.open(image);
+        bmp = image.toPixelMatrix();
+
+
+        //convert pixel to grey scale equivalents
+        for( ; ; )
+        {
+
+        }
+        //find average of component red, green and blue
+        //assign rgb average value
+        //repeat until all pixels are converted in picture
+
+        //save new picture as oldtimey.bmp
+
+        image.fromPixelMatrix(bmp);
+        image.save("machupicchu.bmp");
+
+
+        return 0;
 }
+
+
+
+
+
+
+
