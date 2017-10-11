3ffb4c4ffbffcd8f6c38cad0636cd88d8cc64f9e
KittyCervantes <bcervantes9@ecc-jaguar.csuchico.edu>
2017-10-11 16:25:24 -0700

    can run and save new image now

--- main.cpp
+++ main.cpp
@@ -21,30 +21,27 @@ int main()
         do{
                 cout<<"Please upload valid 24 bit depth bmp image: "<<endl;
                 cin>>usrimage;
-        bmpimage.open(usrimage);
+                bmpimage.open(usrimage);
         }
-        while(bmpimage.isImage() == false); //check to see if thats right
- 
-        bmp = bmpimage.toPixelMatrix(); //"bmpimage" or "image" here?
+        while(bmpimage.isImage() == false); //makes sure it's NOT a valid image
+
+        bmp = bmpimage.toPixelMatrix(); //converts to bitmap
 
         //convert pixel to grey scale equivalents
 
         for(int row=0;row<usrimage.size();row++)
         {
 
-        
                 for(int column=0;column<usrimage.size();column++)
-                {
-                rgb = bmp[row][column]; 
-                color=(rgb.red + rgb.green + rgb.blue)/3;
-                
-                               
+                {//find average of components red, green, and blue
+                        //assign rgb average value
+                        rgb=bmp[row][column]; 
+                        color = (rgb.red+rgb.green+rgb.blue)/3;
+                        bmp[row][column]=rgb;    
                 }
-       
-       }
-        //find average of component red, green and blue
-        //assign rgb average value
-        //repeat until all pixels are converted in picture
+            cout<<endl;
+                //repeat until all pixels are converted in picture
+        }
 
         //save new picture as oldtimey.bmp
         bmpimage.fromPixelMatrix(bmp);
